e7fc40b8ea92b57030c984ac53b8f1d7dfc152e6
Chaudhary1010 <sakshamchaudhary1010@gmail.com>
2024-06-29 21:38:58 +0530

    Initial commit

---
+++ website-project/scripts.js
@@ -0,0 +1,9 @@
+// Simulating earnings
+let todayEarnings = 0.4;
+let totalEarnings = 0.4;
+
+// Display earnings on dashboard
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('today-earnings').innerText = todayEarnings;
+    document.getElementById('total-earnings').innerText = totalEarnings;
+});
